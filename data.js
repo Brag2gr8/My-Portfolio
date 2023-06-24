f8013973af921fca3beb7d17f694add312ec800f
Emmanuel Ogbuzuru <58727101+Brag2gr8@users.noreply.github.com>
2023-06-24 13:47:51 +0100

    update site
    made adequate changes to mobile view

---
+++ data.js
@@ -0,0 +1,94 @@
+export const projects = [
+    {
+        id: 0,
+        name: "MovieLife",
+        image: "movielife",
+        desc: "MovieLife is a movie discovery platform that helps users explore, find, and track their favorite films.",
+        github: "https://github.com/Brag2gr8/MovieLife",
+        demo:  "https://movielife.netlify.app",
+        skills: ["figma", "react", "css", "firestore"]
+    },
+    {
+        id: 0,
+        name: "MovieLife",
+        image: "movielife",
+        desc: "MovieLife is a movie discovery platform that helps users explore, find, and track their favorite films.",
+        github: "https://github.com/Brag2gr8/MovieLife",
+        demo:  "https://movielife.netlify.app",
+        skills: ["figma", "react", "css", "firestore"]
+    },
+    {
+        id: 0,
+        name: "MovieLife",
+        image: "movielife",
+        desc: "MovieLife is a movie discovery platform that helps users explore, find, and track their favorite films.",
+        github: "https://github.com/Brag2gr8/MovieLife",
+        demo:  "https://movielife.netlify.app",
+        skills: ["figma", "react", "css", "firestore"]
+    },
+    {
+        id: 0,
+        name: "MovieLife",
+        image: "movielife",
+        desc: "MovieLife is a movie discovery platform that helps users explore, find, and track their favorite films.",
+        github: "https://github.com/Brag2gr8/MovieLife",
+        demo:  "https://movielife.netlify.app",
+        skills: ["figma", "react", "css", "firestore"]
+    },
+]
+
+export const skills = [
+    [
+        {
+            name: "HTML5",
+            icon: "html"
+        },
+        {
+            name: "CSS3",
+            icon: "css"
+        },
+        {
+            name: "JAVASCRIPT",
+            icon: "js"
+        },
+        {
+            name: "REACT",
+            icon: "react"
+        },
+        {
+            name: "FIGMA",
+            icon: "figma"
+        },
+        {
+            name: "GIT",
+            icon: "git"
+        },
+        {
+            name: "FIRESTORE",
+            icon: "firestore"
+        },
+    ],
+    [
+        {
+            name: "TYPESCRIPT",
+            icon: "ts"
+        },
+        {
+            name: "NODE",
+            icon: "node"
+        },
+        {
+            name: "SOLIDITY",
+            icon: "solidity"
+        },
+        {
+            name: "ETHER",
+            icon: "ether"
+        },
+        {
+            name: "WEB3",
+            icon: "web3"
+        },
+    ]
+
+]
---
+++ index.js
@@ -0,0 +1,87 @@
+import { projects, skills } from "/data.js";
+
+const usingNow = document.getElementById("using-now")
+const learning = document.getElementById("learning")
+const allProjects = document.getElementById("all-projects-container")
+
+
+const getUsingNowHtml = () => {
+    let html = ""
+
+    skills[0].map(skill => {
+        html += `
+            <div>
+                <img src="/images/${skill.icon}.png" alt="${skill.icon} logo" >
+                <p>${skill.name}</p>
+            </div>
+        `
+    })
+
+    return html
+}
+
+const getLearningHtml = () => {
+    let html = ""
+
+    skills[1].map(skill => {
+        html += `
+            <div>
+                <img src="/images/${skill.icon}.png" alt="${skill.icon} logo" >
+                <p>${skill.name}</p>
+            </div>
+        `
+    })
+
+    return html
+}
+
+
+const getProjectsHtml = () => {
+    let html = ""
+    
+    projects.map(project => {
+        const {name, image, desc, github, demo, skills} = project
+        
+        let skillHtml = ""
+        
+        skills.map(skill => {
+            skillHtml += `
+                <img src="./images/${skill}.png" alt="${skill} logo" >
+            `
+        })
+    
+        html += `
+            <div class="project-container">
+                <img src="./images/${image}.png">
+                <div class="project-info">
+                    <h2>${name}</h2>
+                    <div class="project-tools-container">
+                        ${skillHtml}
+                    </div>
+                    <p>${desc}</p>
+                    <div class="project-links">
+                        <a href="${github}" target="_blank"><div>
+                            <p>Code</p>
+                            <img class="github" src="./images/github.png" alt="github">
+                        </div></a>
+                        <a href="${demo}" target="_blank"><div>
+                            <p>Demo</p>
+                            <img class="link" src="./images/link.png" alt="link">
+                        </div></a>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+    
+    return html
+}
+
+
+const render = () => {
+    usingNow.innerHTML = getUsingNowHtml()
+    learning.innerHTML = getLearningHtml()
+    allProjects.innerHTML = getProjectsHtml()
+}
+
+render()
